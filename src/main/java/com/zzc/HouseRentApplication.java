c3bb1020c20e5227f4cab389e0c0231dc74b6f14
zzc <3210696396@qq.com>
2023-06-19 11:12:15 +0800

    房屋租赁系统后端

---
+++ src/main/java/com/zzc/HouseRentApplication.java
@@ -0,0 +1,13 @@
+package com.zzc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseRentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseRentApplication.class, args);
+    }
+
+}
